28c363dc5dd7db97662269d111472095eca435ec
Vitor Paiva <vitor.paiva@vitor.paiva>
2022-06-03 14:17:28 -0300

    Projeto inicial|

---
+++ programacao-funcional/README.md
@@ -0,0 +1,6 @@
+# programacao-funcional
+
+docker build -t programacao-funcional .
+
+docker run -it --rm programacao-funcional clj
+
---
+++ programacao-funcional/test/programacao_funcional/core_test.clj
@@ -0,0 +1,7 @@
+(ns programacao-funcional.core-test
+  (:require [clojure.test :refer :all]
+            [programacao-funcional.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
