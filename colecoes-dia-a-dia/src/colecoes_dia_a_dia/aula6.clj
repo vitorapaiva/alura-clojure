dc4c5abf89a07b5ea2558646f761256b25ffda11
Vitor Paiva <vitor.paiva@vitor.paiva>
2022-06-06 20:02:19 -0300

    Fim do curso

--- colecoes-dia-a-dia/src/colecoes_dia_a_dia/aula6.clj
+++ colecoes-dia-a-dia/src/colecoes_dia_a_dia/aula6.clj
@@ -26,4 +26,11 @@
   (println "resto" (rest resumo))
   (println "total" (count resumo))
   (println "class" (class resumo))
-  (println "nth 1" (nth resumo 1))   )
+  (println "nth 1" (nth resumo 1))
+  (println "take" (take 2 resumo )))
+
+(let [pedidos (l.db/todos-os-pedidos)
+      resumo (resumo-por-usuario-ordenado pedidos)]
+  (println "quem gastou mais que 500" (filter #(> (:preco-total %) 500) resumo))
+  (println "alguem gastou mais que 500" (some #(> (:preco-total %) 500) resumo)))
+
