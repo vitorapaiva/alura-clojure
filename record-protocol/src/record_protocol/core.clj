5d83d9a824d6e61faaf7bea64c2b409b136337ab
Vitor Paiva <vitor.paiva@vitor.paiva>
2022-06-10 17:35:35 -0300

    Final do outro curso e inicio desse

---
+++ record-protocol/src/record_protocol/core.clj
@@ -0,0 +1,6 @@
+(ns record-protocol.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ record-protocol/test/record_protocol/core_test.clj
@@ -0,0 +1,7 @@
+(ns record-protocol.core-test
+  (:require [clojure.test :refer :all]
+            [record-protocol.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
